77f5b1d4bca7877bc74d8f195d6ab7df48d6ed6b
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-08-13 17:25:13 +0600

    Add Login page

---
+++ src/Pages/Login.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { BiGlobe } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import InputField from "../Components/InputField/InputField";
+import PasswordInput from "../Components/InputField/PasswordInput";
+import Selector from "../Components/Selector/Selector";
+import Logo from "../Components/assets/Icons/Logo.svg";
+import Login_img from "../Components/assets/Images/Login.png";
+
+function LogIn() {
+  const options = [
+    { value: "English", label: "English" },
+    { value: "French", label: "French" },
+    { value: "Spanish", label: "Spanish" },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+    console.log(`Selected: ${selectedOption.label}`);
+  };
+
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePassChange = (event) => {
+    setPass(event.target.value);
+  };
+
+  return (
+    <div className="logIn_page">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <div className="login_form">
+              <div className="login_header">
+                <img src={Logo} alt="Logo" className="logo" />
+                <div className="lang_selector">
+                  <BiGlobe className="icons" />
+                  <Selector
+                    options={options}
+                    onChange={handleChange}
+                    placeholder="English"
+                    value={selectedOption}
+                  />
+                </div>
+              </div>
+              <div className="form_area">
+                <div className="mb-5">
+                  <h1>Sign in to Modifii</h1>
+                  <p className="mt-2 desc">
+                    Simple and secure all-in-one management software
+                  </p>
+                </div>
+                <form action="" className="input_form">
+                  <InputField
+                    type="email"
+                    value={name}
+                    placeholder="Username or email"
+                    onChange={handleNameChange}
+                  />
+                  <PasswordInput
+                    type="password"
+                    value={pass}
+                    placeholder="Password"
+                    onChange={handlePassChange}
+                  />
+                  <div className="forget_area">
+                    <span className="remember_me">
+                      <input type="checkbox" className="redio_input" />
+                      <p>Remember me</p>
+                    </span>
+                    <Link to="#" className="fuP">
+                      Forgot Password?
+                    </Link>
+                  </div>
+                  <Link to="/dashboard">
+                    <button className="btn">Sign In</button>
+                  </Link>
+                </form>
+              </div>
+              <div className="logIn_footer">
+                <Link to="">Privacy Policy</Link>
+                <p>Copyright 2024</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <div className="login_img">
+              <img src={Login_img} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;
