84e345931fd70d70b8f8d486f093ab79df475e1c
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-08-14 11:31:22 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MainLayout from "./Components/Layout/MainLayout";
 import Dashboard from "./Pages/Dashboard";
@@ -6,21 +6,6 @@ import LogIn from "./Pages/Login";
 import Contacts from "./Pages/Contacts";
 
 function App() {
-  const [activeKey, setActiveKey] = useState(
-    localStorage.getItem("activeKey") || "dashboard"
-  );
-
-  useEffect(() => {
-    localStorage.setItem("activeKey", activeKey);
-  }, [activeKey]);
-
-  useEffect(() => {
-    // Set default activeKey to "dashboard" on first load
-    if (!localStorage.getItem("activeKey")) {
-      setActiveKey("dashboard");
-    }
-  }, []);
-
   return (
     <Router>
       <Routes>
